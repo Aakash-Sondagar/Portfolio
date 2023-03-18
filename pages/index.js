71242d3fd5d3eb7bc42ef7d79dc24cdc2c574be5
Aakash-Sondagar <aakashsondar@gmail.com>
2023-03-18 15:22:31 +0530

    Integrating Chakra UI

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
+
 const Page = () => {
-    return <div>Hello</div>
+    return (
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello, I'm a Software developer based in India!
+            </Box>
+
+            <Box display={{md : 'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Aakash Sondagar
+                    </Heading>
+                    <p>A Software Engineer</p>
+                </Box>
+            </Box>
+        </Container>
+    )
 }
 
 export default Page
