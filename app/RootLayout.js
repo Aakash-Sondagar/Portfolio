cb9287d7812704e9a4ba50c626036e0211d48a32
Aakash-Sondagar <aakashsondar@gmail.com>
2024-05-24 23:54:43 +0530

    Changes

--- app/RootLayout.js
+++ app/RootLayout.js
@@ -1,16 +1,5 @@
 import Head from "next/head";
-import "./globals.css";
-
-export const metadata = {
-  title: "Aakash's Portfolio",
-  description:
-    "Welcome to Aakash's professional portfolio website. Explore projects, skills, and get in touch.",
-  keywords:
-    "Aakash, Aakash Sondagar, Aakash Sondagar portfolio, portfolio, software engineer, projects, contact",
-  author: "Aakash Sondagar",
-  url: "https://your-portfolio-url.com",
-  image: "https://your-portfolio-url.com/your-image.jpg",
-};
+import { metadata } from "./layout";
 
 export default function RootLayout({ children }) {
   return (
