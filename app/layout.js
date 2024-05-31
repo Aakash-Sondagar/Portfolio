dbdeeadb08a7054b5bf8b4d13e447befddd2de40
Aakash-Sondagar <aakashsondar@gmail.com>
2024-05-31 20:22:34 +0530

    next-seo Added

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,17 @@
 import Head from "next/head";
 import "./globals.css";
 
-export const metadata = {
-  title: "Aakash's Portfolio",
-  description:
-    "Welcome to Aakash's professional portfolio website. Explore projects, skills, and get in touch.",
-  keywords:
-    "Aakash, Aakash Sondagar, Aakash portfolio, Aakash Sondagar portfolio, portfolio, portfolio website, software engineer, software development engineer, full-stack developer, projects, contact",
-  author: "Aakash Sondagar",
-  url: "https://aakashsondagar.vercel.app",
-};
-
 export default function RootLayout({ children }) {
+  const metadata = {
+    title: "Aakash's Portfolio",
+    description:
+      "Welcome to Aakash's professional portfolio website. Explore projects, skills, and get in touch.",
+    keywords:
+      "Aakash, Aakash Sondagar, Aakash portfolio, Aakash Sondagar portfolio, portfolio, portfolio website, software engineer, software development engineer, full-stack developer, projects, contact",
+    author: "Aakash Sondagar",
+    url: "https://aakashsondagar.vercel.app",
+  };
+
   return (
     <html lang="en">
       <Head>
@@ -21,14 +21,12 @@ export default function RootLayout({ children }) {
         <meta name="author" content={metadata.author} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        {/* Open Graph Meta Tags */}
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:url" content={metadata.url} />
         <meta property="og:image" content={metadata.image} />
         <meta property="og:type" content="website" />
 
-        {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={metadata.title} />
         <meta name="twitter:description" content={metadata.description} />
