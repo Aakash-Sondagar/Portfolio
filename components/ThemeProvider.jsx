ae9de6f96b7faa37e328be45f5a652075c25462f
Aakash Sondagar <89018309+Aakash-Sondagar@users.noreply.github.com>
2025-06-15 00:27:29 +0530

    Proper dappled light effects with leaves.png and window shutters

--- components/ThemeProvider.jsx
+++ components/ThemeProvider.jsx
@@ -1,26 +1,58 @@
 "use client";
 
-const ThemeProvider = () => {
+const ThemeProvider = ({ children }) => {
   return (
-    <div className="atmospheric-container">
-      {/* Dappled Light Effect */}
-      <div className="dappled-light"></div>
-      
-      {/* Leaves Effect */}
-      <div className="leaves-container">
-        <div className="leaf"></div>
-        <div className="leaf"></div>
-        <div className="leaf"></div>
-        <div className="leaf"></div>
-        <div className="leaf"></div>
-        <div className="leaf"></div>
+    <div>
+      {/* Dappled Light Effects */}
+      <div id="dappled-light">
+        <div id="glow"></div>
+        <div id="glow-bounce"></div>
+        <div className="perspective">
+          <div id="leaves"></div>
+          <div id="blinds">
+            <div className="shutters">
+              {[...Array(10)].map((_, i) => (
+                <div key={i} className="shutter"></div>
+              ))}
+            </div>
+            <div className="vertical">
+              <div className="bar"></div>
+              <div className="bar"></div>
+            </div>
+          </div>
+        </div>
+        <div id="progressive-blur">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
-      
-      {/* Window Shutters */}
-      <div className="shutters-container">
-        <div className="shutter shutter-left"></div>
-        <div className="shutter shutter-right"></div>
-      </div>
+
+      {/* SVG Filter for Wind Effect */}
+      <svg width="0" height="0" style={{ position: 'absolute' }}>
+        <defs>
+          <filter id="wind">
+            <feTurbulence 
+              baseFrequency="0.02 0.1" 
+              numOctaves="3" 
+              result="turbulence"
+            >
+              <animate 
+                attributeName="baseFrequency" 
+                values="0.02 0.1;0.04 0.12;0.02 0.1" 
+                dur="8s" 
+                repeatCount="indefinite"
+              />
+            </feTurbulence>
+            <feDisplacementMap 
+              in="SourceGraphic" 
+              in2="turbulence" 
+              scale="2"
+            />
+          </filter>
+        </defs>
+      </svg>
     </div>
   );
 };
