85f460c76286df9c9f68551e22909efcb32dd56e
Aakash Sondagar <89018309+Aakash-Sondagar@users.noreply.github.com>
2025-06-15 23:50:19 +0530

    Fix Jest MDX file resolution

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'mdx'],
   moduleNameMapping: {
     '^@/(.*)$': '<rootDir>/$1',
   },
